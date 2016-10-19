020f077e5271ef82360d3497678a25c2dc4ab41b
chengchao <1244053432@qq.com>
2016-10-19 11:37:14 +0800

    Revert "Revert "create new file""
    This reverts commit 02813f332575bc9d757f4bd28ce8c115577cf1cb.

---
+++ 天气预报/天气预报/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  天气预报
+//
+//  Created by chengchao on 15/8/11.
+//  Copyright (c) 2015年 chengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 天气预报/天气预报/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  天气预报
+//
+//  Created by chengchao on 15/8/11.
+//  Copyright (c) 2015年 chengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
